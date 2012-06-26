45fd166f9012c96ac16a273cdb68844836398f52
Kohe Tokoi <tokoik@gmail.com>
2012-06-26 23:50:24 +0900

    update dependency

---
+++ winter02.d
@@ -0,0 +1,5 @@
+Bone.o: Bone.cpp Bone.h
+Matrix.o: Matrix.cpp Matrix.h
+disseminate.o: disseminate.cpp disseminate.h
+main.o: main.cpp Matrix.h Bone.h disseminate.h shadersource.h
+shadersource.o: shadersource.cpp shadersource.h
